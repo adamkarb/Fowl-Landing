fed2ecdf9b8c9a40b1110da563ebd6e85f8483e5
Adam Karbiener <adamkarb@gmail.com>
2015-03-12 18:45:08 -0400

    If only passing an object to a variable outside of a function was easier for a noob

--- src/app/weather/weather.controller.js
+++ src/app/weather/weather.controller.js
@@ -2,6 +2,25 @@
 
 angular.module('waterfowlGulpAngular')
 
-.controller('weatherCtrl', function($http) {
-    $http.get("https://api.openweathermap.org/data/2.5/weather?&APPID=477a3d75817905807c66907f0ec12162")
-});
+.controller('weatherCtrl', [ '$http', function($http) {
+    
+    var self = this;
+    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=orlando&type=like&lat&long&cnt=7&speed&all&icon&APPID=477a3d75817905807c66907f0ec12162")
+    .success(function(data){
+        self = data;
+        return self;
+        console.log(self);
+        
+    })
+    .error(function(){
+        console.log('you suck')
+    })
+    console.log(self);
+    
+}]);
+
+
+
+//https://api.openweathermap.org/data/2.5/forecast?q=orlando&type=like&APPID=477a3d75817905807c66907f0ec12162
+
+//Parameters: name,lat,lon,sunrise,sunset,temp,speed,all(cloud%),description,icon
