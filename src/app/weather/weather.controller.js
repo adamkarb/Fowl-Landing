b093fc96aa39bacc442fc6ae6014d1cf3791c59a
Adam Karbiener <adamkarb@gmail.com>
2015-03-12 16:09:36 -0400

    Snagged myself a key from openweathermap.  Next step is to finish up the parameter string

--- src/app/weather/weather.controller.js
+++ src/app/weather/weather.controller.js
@@ -2,6 +2,6 @@
 
 angular.module('waterfowlGulpAngular')
 
-.controller('weatherCtrl', function() {
-    
+.controller('weatherCtrl', function($http) {
+    $http.get("https://api.openweathermap.org/data/2.5/weather?&APPID=477a3d75817905807c66907f0ec12162")
 });
