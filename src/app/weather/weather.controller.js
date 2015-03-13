dc7614cc67f8cd367cb8ed763e197bd04d58aa50
Adam Karbiener <adamkarb@gmail.com>
2015-03-13 17:53:14 -0400

    Made a factory that returns jsonp for the windermere,Fl weather location.  Small victories, people...  Next stop is harnessing it and making it usable on the page

--- src/app/weather/weather.controller.js
+++ src/app/weather/weather.controller.js
@@ -2,20 +2,19 @@
 
 angular.module('waterfowlGulpAngular')
 
-.config(function(RestangularProvider){
-    RestangularProvider.setBaseUrl('http://api.openweathermap.org/data/2.5/forecast/')
-    RestangularProvider.setDefaultRequestParams({type: 'like'})
-    RestangularProvider.setDefaultRequestParams({cnt: '7'})
-    RestangularProvider.setDefaultRequestParams({APPID: '477a3d75817905807c66907f0ec12162'})
-})
 
 .controller('weatherCtrl', function($http, weatherFactory) {
+    
+//    $http.jsonp('https://api.forecast.io/forecast/122e62eac5a1a3d44186abef5e1cabd3/-28.4158,81.2989?callback=JSON_CALLBACK')
+//    .success(function(data){
+//        console.log(data);
+//    })
+    
     
     
     var self = this;
     weatherFactory.then(function(data){
-        self.lat = data.city.coord.lat;
-        console.log(self.lat);
+        console.log(data);
         
     });
     
@@ -24,8 +23,10 @@ angular.module('waterfowlGulpAngular')
     
 }) //END CONTROLLER
 
-.factory('weatherFactory', function(Restangular){
-   return Restangular.one('daily').get({q: 'jacksonville'})
+.factory('weatherFactory', function($http){
+   return $http.jsonp('https://api.forecast.io/forecast/122e62eac5a1a3d44186abef5e1cabd3/-28.4158,81.2989?callback=JSON_CALLBACK').success(function(data){
+       console.log('Factory is working')
+   })
 
 })
 
