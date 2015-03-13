659bb8048b7d4f5d6149469996ff6f149b47f76d
Adam Karbiener <adamkarb@gmail.com>
2015-03-13 13:15:51 -0400

    Started using restangular for the api call.  About to nix some of the old stuff

--- src/app/weather/weather.controller.js
+++ src/app/weather/weather.controller.js
@@ -2,29 +2,69 @@
 
 angular.module('waterfowlGulpAngular')
 
-.controller('weatherCtrl', function($http) {
+.config(function(RestangularProvider){
+    RestangularProvider.setBaseUrl('http://api.openweathermap.org/data/2.5/forecast/')
+    RestangularProvider.setDefaultRequestParams({type: 'like'})
+    RestangularProvider.setDefaultRequestParams({cnt: '7'})
+    RestangularProvider.setDefaultRequestParams({APPID: '477a3d75817905807c66907f0ec12162'})
+})
+
+.controller('weatherCtrl', function($http, weatherFactory) {
     
     //function getCoord(){};
 //    on ng-submit;
-    var self = this;
-    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=birmingham&type=like&lat&long&cnt=7&speed&all&icon&APPID=477a3d75817905807c66907f0ec12162")
-    .success(function(data){
-        self.local = data;
-        
-        
-        console.log(self);
-        
-    })
-    .error(function(){
-        console.log('you suck')
-    })
-   
-    //declare self.lat = data.local.city.coord.lat
-    //declare self.lon = data.local.city.coord.lon
-    //funnel ^^getCoord lat+lon results into forecast.io api call
-    //for the sake of getting the real good data out;
+//    var self = this;
+//    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=miami&type=like&cnt=7&APPID=477a3d75817905807c66907f0ec12162")
+//    .success(function(data){
+//        self.local = data;
+//        self.city = data.city.name;
+//        self.lat = data.city.coord.lat;
+//        self.lon = data.city.coord.lon;
+//        
+//        console.log(self);
+//        
+//    })
+//    .error(function(){
+//        console.log('you suck')
+//    })
     
-});
+    var self = this;
+    weatherFactory.then(function(data){
+        self.lat = data.city.coord.lat;
+        console.log(self.lat);
+    })
+    
+    
+    
+    
+}) //END CONTROLLER
+
+.factory('weatherFactory', function(Restangular){
+   return Restangular.one('daily').get({q: 'jacksonville'})
+    
+    
+    
+    
+    
+//    var local, city, lat, lon;
+//    
+//    return $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=miami&type=like&lat&long&cnt=7&speed&all&icon&APPID=477a3d75817905807c66907f0ec12162")
+////    .success(function(data){
+//        var local = data;
+//        var city = data.city.name;
+//        var lat = data.city.coord.lat;
+//        var lon = data.city.coord.lon;
+//        
+//    })
+//    .error(function(){
+//        console.log('you suck')
+//    })
+    
+    
+})
+
+
+;
 
 
 // APIKEY forecast.io  `only written as numbers`= 122e62eac5a1a3d44186abef5e1cabd3
