4948bbde96ffaabc95b3d549c90198be43ef3ca4
Adam Karbiener <adamkarb@gmail.com>
2015-03-12 23:13:41 -0400

    The first api call works great. As a total hack feature, I plan to funnel the api call with coordinates from the first api into the darksky api in order that I may have a user input a city by name and get data out.  I have laid out a little plan to get the ball rolling for next time.

--- src/app/weather/weather.controller.js
+++ src/app/weather/weather.controller.js
@@ -2,25 +2,30 @@
 
 angular.module('waterfowlGulpAngular')
 
-.controller('weatherCtrl', [ '$http', function($http) {
+.controller('weatherCtrl', function($http) {
     
+    //function getCoord(){};
+//    on ng-submit;
     var self = this;
-    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=orlando&type=like&lat&long&cnt=7&speed&all&icon&APPID=477a3d75817905807c66907f0ec12162")
+    $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=birmingham&type=like&lat&long&cnt=7&speed&all&icon&APPID=477a3d75817905807c66907f0ec12162")
     .success(function(data){
-        self = data;
-        return self;
+        self.local = data;
+        
+        
         console.log(self);
         
     })
     .error(function(){
         console.log('you suck')
     })
-    console.log(self);
+   
+    //declare self.lat = data.local.city.coord.lat
+    //declare self.lon = data.local.city.coord.lon
+    //funnel ^^getCoord lat+lon results into forecast.io api call
+    //for the sake of getting the real good data out;
     
-}]);
+});
 
 
+// APIKEY forecast.io  `only written as numbers`= 122e62eac5a1a3d44186abef5e1cabd3
 
-//https://api.openweathermap.org/data/2.5/forecast?q=orlando&type=like&APPID=477a3d75817905807c66907f0ec12162
-
-//Parameters: name,lat,lon,sunrise,sunset,temp,speed,all(cloud%),description,icon
