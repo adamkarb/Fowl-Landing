24126099448ecb5412ec4e830f21f037b6eec13f
Adam Karbiener <adamkarb@gmail.com>
2015-03-17 13:59:07 -0400

    So you can now drop pins on a map.  The world is my oyster

--- src/app/maps/map.controller.js
+++ src/app/maps/map.controller.js
@@ -10,8 +10,8 @@ angular.module('waterfowlGulpAngular')
 
   
     
-    
-.controller('gMap', function ($scope) {
+//This library requires scope. Get with the times
+.controller('gMap', function ($scope, uiGmapGoogleMapApi) {
     console.log('gMap controller works');
     
     $scope.map = {
@@ -22,18 +22,51 @@ angular.module('waterfowlGulpAngular')
         zoom: 5
     };
     
-    $scope.markers = [
-        {
-          id: 1,  
-          latitude: "28.4158",
-          longitude: "-81.2989" 
-        },
-        {
-            id: 2,
-            latitude: "29",
-            longitude: "-84"
+    
+    $scope.markers = [];
+    
+    $scope.events = {
+        click: function (map, eventName, handlerArgs) {
+            $scope.$apply(function () {
+                console.log($scope.markers)
+                //console.log(handlerArgs)
+                $scope.markers.push({
+                    id: $scope.markers.length,
+                    latitude: handlerArgs[0].latLng.lat(),
+                    longitude: handlerArgs[0].latLng.lng(),
+                    options: {
+                        animation: api.Animation.DROP,
+                        title: handlerArgs[0].latLng.toUrlValue()
+              
+                    }
+                });
+            });
         }
-    ];
+    };
+
+    var api;
+    uiGmapGoogleMapApi.then(function(googleMaps) {
+        api = googleMaps;
+    });
+    
+    
+//    $scope.markers = [
+//        {
+//            id: 1,  
+//            latitude: "28.4158",
+//            longitude: "-81.2989",
+//            options: {
+//                labelContent: "SwaytheDev",
+//                draggable: true,
+//                crossOnDrag: false
+//            }
+//        },
+//        {
+//            id: 2,
+//            latitude: "29",
+//            longitude: "-84"
+//        }
+//    ];
     
 
 });
