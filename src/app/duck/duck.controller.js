cb43a365ba9ccaccd72394b71e2fc04c746852d7
Adam Karbiener <adamkarb@gmail.com>
2015-03-20 11:42:51 -0400

    updating controllers

--- src/app/duck/duck.controller.js
+++ src/app/duck/duck.controller.js
@@ -1,6 +1,26 @@
-angular.module('Waterfowl-gulp-angular', [])
+angular.module('waterfowlGulpAngular', [])
 
 .controller('DuckCtrl', function(){
     
+    this.list = duckArray;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    duckArray = [
+        {
+            name: "Mallard",
+            description: "This is a fun duck to shoot"
+        },
+        {
+            name: "Black Duck",
+            description: "Looks like a mallard but darker"
+        }
+    ]
     
 });
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('waterfowlGulpAngular')
+
   .controller('MainCtrl', function () {
-    
+    console.log('Hello')
   });
 
 
